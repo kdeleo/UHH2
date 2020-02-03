e79bc8d085ed5bb3bb216d31d59ca6d2dfd63e75
Ksenia de Leo <ksenia.de.leo@desy.de>
2020-02-03 14:27:21 +0100

    convert vars for QCD

---
+++ MLCorner/InputsPreparation/UHH2toNumpyQCD/python/constants.py
@@ -0,0 +1,7 @@
+#path to the python code
+path_MLDIR = '/nfs/dust/cms/user/deleokse/analysis/CMSSW_10_2_10/src/UHH2/MLCorner/InputsPreparation/UHH2toNumpyQCD/'
+#path to UHH2 output, used as input for convertion
+fullsel_path = '/nfs/dust/cms/user/deleokse/analysis/ZPrime_2017/Preselection_2017v2/' 
+#path to store converted numpy files 
+outpath = '/nfs/dust/cms/user/deleokse/analysis/MLInputs_numpy_0302/' 
+
---
+++ MLCorner/InputsPreparation/UHH2toNumpyQCD/steer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import subprocess
+import os
+import time
+
+from python.ModuleRunner import *
+from python.constants import *
+
+
+"""This is macro to steer Root to Numpy conversion. """
+
+
+#paths are set in constants.py
+ModuleRunner = ModuleRunner(path_MLDIR, outpath)
+
+# ---- Macro for ML inputs preparation ----
+#names of the process, e.g part after uhh2.AnalysisModuleRunner. in the input file name
+procnames = ['QCD_HT500to700_2017v2']
+#name of branches to be skipped in conversion
+## exact names
+unwanted_exact_tags = ['Mttbar', 'NPV', 'event', 'isRealData', 'luminosityBlock', 'met_pt', 'passEcalBadCalib', 'rec_chi2','rho', 'run', 'GenParticles', 'offlineSlimmedPrimaryVertices', 'year']
+## partial names to exclude common set of variables at once
+unwanted_tags = ['ak4jet1_', 'ak8jet1_', 'beamspot_', 'weight_pu', 'prefiringWeight', 'trigger', 'gen', 'jets', 'slimmed', 'm_', 'offlineSlimmedPrimaryVertices_', 'GenParticles_',
+                  'lep1_', 'weight_']
+
+syst_vars = ['NOMINAL']
+for syst_var in syst_vars:
+    print'--- Convert inputs for ',syst_var,' variation ---'
+    ModuleRunner.ReadoutMLVariables(procnames=procnames,unwanted_tags=unwanted_tags, unwanted_exact_tags=unwanted_exact_tags,syst_var=syst_var)
+
