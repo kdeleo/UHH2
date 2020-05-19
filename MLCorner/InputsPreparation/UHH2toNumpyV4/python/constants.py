682b706faaff53e70dafd66e14c8d7c51d51a661
Ksenia de Leo <ksenia.de.leo@desy.de>
2020-05-19 11:04:23 +0200

    prepare inputs with new vars

--- MLCorner/InputsPreparation/UHH2toNumpyV4/python/constants.py
+++ MLCorner/InputsPreparation/UHH2toNumpyV4/python/constants.py
@@ -1,7 +1,7 @@
 #path to the python code
 path_MLDIR = '/nfs/dust/cms/user/deleokse/analysis/CMSSW_10_2_10/src/UHH2/MLCorner/InputsPreparation/UHH2toNumpyV4/'
 #path to UHH2 output, used as input for convertion
-fullsel_path = '/nfs/dust/cms/user/deleokse/analysis/ZPrime_2017/Analysis_NNvars_V4/muon/' 
+fullsel_path = '/nfs/dust/cms/user/deleokse/analysis/ZPrime_2017/Analysis_NNvars_V4_1705/muon/' 
 #path to store converted numpy files 
-outpath = '/nfs/dust/cms/user/deleokse/analysis/MLInputs_numpy_V4_1305/' 
+outpath = '/nfs/dust/cms/user/deleokse/analysis/MLInputs_numpy_V4_1705/' 
 
--- MLCorner/InputsPreparation/UHH2toNumpyV4/steer.py
+++ MLCorner/InputsPreparation/UHH2toNumpyV4/steer.py
@@ -16,15 +16,14 @@ ModuleRunner = ModuleRunner(path_MLDIR, outpath)
 
 # ---- Macro for ML inputs preparation ----
 #names of the process, e.g part after uhh2.AnalysisModuleRunner. in the input file name
-procnames = ['QCD','Zprime']
-#procnames = ['TTbar', 'ST', 'WJets_1', 'WJets_2','WJets_3', 'DY']
+procnames = ['TTbar', 'ST', 'WJets', 'DY', 'Zprime']
 #procnames = ['ZprimeToTTJet_M1000_2017v2', 'ZprimeToTT_M1000_W100_2017v2', 'ZprimeToTT_M3000_W900_2017v2']
 #name of branches to be skipped in conversion
 ## exact names
 unwanted_exact_tags = ['Mttbar', 'NPV', 'event', 'isRealData', 'luminosityBlock', 'met_pt', 'passEcalBadCalib','rho', 'run', 'GenParticles', 'offlineSlimmedPrimaryVertices', 'year', 'rec_chi2']
 ## partial names to exclude common set of variables at once
 unwanted_tags = ['ak4jet1_', 'ak8jet1_', 'beamspot_', 'weight_pu', 'prefiringWeight', 'trigger', 'gen', 'jets', 'slimmed', 'm_', 'offlineSlimmedPrimaryVertices_', 'GenParticles_',
-                  'lep1_', 'weight_', 'Ele', 'Ak4_j7', 'Ak4_j8', 'Ak4_j9', 'Ak4_j10', 'Ak8_j4', 'Ak8_j5']
+                  'lep1_', 'weight_', 'Ele']
 
 syst_vars = ['NOMINAL']
 for syst_var in syst_vars:
