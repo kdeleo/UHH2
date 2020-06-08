e00abdf1157ba3abdc839be0e79f01379c13f30f
Ksenia de Leo <ksenia.de.leo@desy.de>
2020-06-08 15:12:29 +0200

    add variables for all Zprime samples

--- MLCorner/InputsPreparation/UHH2toNumpyV4/steer.py
+++ MLCorner/InputsPreparation/UHH2toNumpyV4/steer.py
@@ -16,7 +16,8 @@ ModuleRunner = ModuleRunner(path_MLDIR, outpath)
 
 # ---- Macro for ML inputs preparation ----
 #names of the process, e.g part after uhh2.AnalysisModuleRunner. in the input file name
-procnames = ['TTbar', 'ST', 'WJets', 'DY', 'Zprime']
+procnames = ['Zprime']
+#procnames = ['TTbar', 'ST', 'WJets', 'DY', 'Zprime']
 #procnames = ['ZprimeToTTJet_M1000_2017v2', 'ZprimeToTT_M1000_W100_2017v2', 'ZprimeToTT_M3000_W900_2017v2']
 #name of branches to be skipped in conversion
 ## exact names
