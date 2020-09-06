25c817e4f6aff03242ff41dc02ed163666184c5c
Ksenia de Leo <ksenia.de.leo@desy.de>
2020-09-06 17:10:19 +0200

    vars after flattening

--- MLCorner/InputsPreparation/UHH2toNumpyV4/steer.py
+++ MLCorner/InputsPreparation/UHH2toNumpyV4/steer.py
@@ -16,8 +16,8 @@ ModuleRunner = ModuleRunner(path_MLDIR, outpath)
 
 # ---- Macro for ML inputs preparation ----
 #names of the process, e.g part after uhh2.AnalysisModuleRunner. in the input file name
-procnames = ['Zprime']
-#procnames = ['TTbar', 'ST', 'WJets', 'DY', 'Zprime']
+procnames = ['WJets-high']
+#procnames = ['TTbar', 'ST', 'WJets-high', 'WJets-low',  'DY', 'Zprime']
 #procnames = ['ZprimeToTTJet_M1000_2017v2', 'ZprimeToTT_M1000_W100_2017v2', 'ZprimeToTT_M3000_W900_2017v2']
 #name of branches to be skipped in conversion
 ## exact names
